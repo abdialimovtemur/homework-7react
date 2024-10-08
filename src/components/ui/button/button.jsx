b94b517e66ddd68656b3a648648545e3617c4739
abdialimovtemur <abdialimovtemur@gmail.com>
2024-10-08 09:13:08 +0500

    first commit

---
+++ src/components/ui/button/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './syle.module.scss'
+
+export const Button = ({ children, variant, type = "button", startIcon, endIcon }) => {
+    return (
+        <div>Button</div>
+    )
+}
+
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio accusantium nemo sit dolores harum repellendus quibusdam minima mollitia! Atque a ratione laborum architecto ipsa rem omnis voluptatum tempore nihil est?</h1>
+        </div>
+    )
+}
